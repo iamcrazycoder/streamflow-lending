b33e3bc5dd742b8f3278bce8a6a7652865f0444d
Nishant Ghodke <iamcrazycoder@gmail.com>
2022-12-25 17:47:43 +0530

    feat: add types

---
+++ src/types/Lend.ts
@@ -0,0 +1,57 @@
+import { Loan, Token, Tx, User } from '@prisma/client'
+import { Mint } from '@solana/spl-token'
+import { PublicKey } from '@solana/web3.js'
+import BigNumber from 'bignumber.js'
+import { TransferTokenResponse } from './Transaction'
+
+interface DisburseLoanArgs {
+    amount: BigNumber
+    mintAddress: PublicKey
+}
+
+type DisburseLoanResponse = TransferTokenResponse & {
+    loanId: Loan['id']
+}
+
+interface IsUserEligibleArgs {
+    mintAddress: PublicKey
+    amount: BigNumber
+}
+
+interface CheckOutstandingAmountArgs {
+    loans: Loan[]
+    tokenInfo: Mint
+    amount: BigNumber
+}
+
+interface GenerateLoanDataArgs {
+    mintAddress: PublicKey
+    amount: BigNumber
+}
+
+interface GetTotalOutstandingAmountArgs {
+    loans: Loan[]
+    mintAddress: PublicKey
+}
+
+interface GetOutstandingLoansArgs {
+    returnReadable?: boolean
+    mintAddress?: PublicKey
+}
+
+interface RelativeLoanData extends Loan {
+    token: Token
+    txs: Tx[]
+    user: User
+}
+
+export {
+    DisburseLoanArgs,
+    DisburseLoanResponse,
+    IsUserEligibleArgs,
+    CheckOutstandingAmountArgs,
+    GenerateLoanDataArgs,
+    GetTotalOutstandingAmountArgs,
+    GetOutstandingLoansArgs,
+    RelativeLoanData,
+}
