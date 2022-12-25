702c06baa3847796da5c35a9bc168494d9efec64
Nishant Ghodke <iamcrazycoder@gmail.com>
2022-12-25 17:19:08 +0530

    feat: add server entrypoint

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import dotenvExpand from 'dotenv-expand'
+import { init } from './config'
+import routers from './routes'
+
+const env = dotenv.config()
+dotenvExpand.expand(env)
+
+init()
+
+const app = express()
+
+app.get('/', (_req, res) => {
+    res.send(
+        'Welcome to Streamflow lending! Refer to the API documentation for usage.'
+    )
+})
+
+if (!process.env.PORT) {
+    throw new Error('PORT is missing in .env file')
+}
+
+// Bind all routes from imported routers
+routers.forEach((route) => {
+    app.use(route.basePath, route.routes)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port: ${process.env.PORT}`)
+})
